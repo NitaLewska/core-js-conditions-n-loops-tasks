3fabb76e82499656ddc2f1ac6638a8697f5c44fa
Anna Muzalevskaya <77303782+NitaLewska@users.noreply.github.com>
2024-01-02 02:59:23 +0300

    feat: solved 7/19 (02:59:15 Tuesday, January 2, 2024)

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -69,8 +69,18 @@ function getMaxNumber(a, b, c) {
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  */
-function canQueenCaptureKing(/* queen, king */) {
-  throw new Error('Not implemented');
+function canQueenCaptureKing(queen, king) {
+  if (Math.abs(king.x - queen.x) === Math.abs(king.y - queen.y)) {
+    return true;
+  }
+  if (king.x === queen.x || king.y === queen.y) {
+    return true;
+  }
+  if (king.x === queen.x || king.y === queen.y) {
+    return true;
+  }
+
+  return false;
 }
 
 /**
@@ -115,8 +125,30 @@ function isIsoscelesTriangle(a, b, c) {
  *  10  => X
  *  26  => XXVI
  */
-function convertToRomanNumerals(/* num */) {
-  throw new Error('Not implemented');
+function convertToRomanNumerals(num) {
+  let input = num;
+  let romanNum = '';
+  while (input >= 10) {
+    romanNum += 'X';
+    input -= 10;
+  }
+  while (input >= 9) {
+    romanNum += 'IX';
+    input -= 9;
+  }
+  while (input >= 5) {
+    romanNum += 'V';
+    input -= 5;
+  }
+  while (input >= 4) {
+    romanNum += 'IV';
+    input -= 5;
+  }
+  while (input >= 1) {
+    romanNum += 'I';
+    input -= 1;
+  }
+  return romanNum;
 }
 
 /**
